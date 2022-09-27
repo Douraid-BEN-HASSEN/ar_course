cd49ddb482dcc67d297c6b925b69bbe6bec7f59a
Lucas AUXACHS <lucas.auxachs@imerir.com>
2022-09-27 11:48:33 +0200

    Add functions to make the json and update ui for subscribe to a run

---
+++ Player/MqttService.cpp
@@ -0,0 +1,82 @@
+#include "MqttService.h"
+
+//#include "src/Kart/Map/Field.h"
+
+MqttService *MqttService::instance()
+{
+    static MqttService instance;
+    return &instance;
+}
+
+
+MqttService::MqttService(QObject *parent): QObject{parent}
+{
+    client = new QMqttClient();
+    client->setHostname(Config.hostname);
+    client->setPort(Config.port);
+    client->setUsername(Config.username);
+    client->setPassword(Config.password);
+
+    subscribes = new QList<QMqttSubscription *>;
+
+    /* -- connect -- */
+    connect(client, &QMqttClient::stateChanged, this, &MqttService::stateChange);
+    connect(client, &QMqttClient::messageReceived, this, &MqttService::receivedMessage);
+
+    client->connectToHost();
+}
+
+void MqttService::publish(QString pTopic, QString pData)
+{
+    QMqttTopicName topic(pTopic);
+    QByteArray data = pData.toUtf8();
+    this->client->publish(topic, data);
+}
+
+/**
+ * SLot of state mqttCLient
+ */
+void MqttService::stateChange() {
+
+    switch (client->state()) {
+        case 0 :
+            qDebug() << "Déconnecté";
+            break;
+        case 1 :
+            qDebug() << "En cours de connexion";
+            break;
+        case 2 :
+            qDebug() << "Connecté";
+
+            subscribes->append(client->subscribe(QString("map")));
+            subscribes->append(client->subscribe(QString("game")));
+
+            break;
+    }
+
+}
+
+/**
+ * callback of message receive
+ * @param message
+ * @param topic
+ */
+void MqttService::receivedMessage(const QByteArray &message, const QMqttTopicName &topic) {
+    qDebug() << message << topic;
+
+    QJsonDocument doc = QJsonDocument::fromJson(message);
+    QJsonObject jsonObject = doc.object();
+
+    if (topic == QString("map")) {
+        //Field *field = Field::instance();
+        //field->deserialize(jsonObject);
+
+        //qDebug() << field->serialize();
+
+    } else if (topic == QString("game")) {
+
+        emit gameUpdated(jsonObject["color"].toString());
+
+    }
+}
+
---
+++ Player/MqttService.h
@@ -0,0 +1,42 @@
+#ifndef MQTTSERVICE_H
+#define MQTTSERVICE_H
+
+#include <QObject>
+#include <QtMqtt/QtMqtt>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+const static struct {
+    const QString hostname = "127.0.0.1";
+    const unsigned short port = 1883;
+    const QString username = "";
+    const QString password = "";
+} Config;
+
+class MqttService: public QObject
+{
+    Q_OBJECT
+public:
+    static MqttService *instance();
+
+    QMqttClient *client;
+    // méthode pour publier
+    void publish(QString pTopic, QString pData);
+
+private:
+    explicit MqttService(QObject * parent = nullptr);
+
+    QList<QMqttSubscription *> *subscribes;
+
+private slots:
+    void stateChange();
+    void receivedMessage(const QByteArray &message, const QMqttTopicName &topic);
+
+public: signals:
+    void mapUpdated(QJsonObject jsonObject);
+    void gameUpdated(QString color);
+
+};
+
+
+#endif // MQTTSERVICE_H
