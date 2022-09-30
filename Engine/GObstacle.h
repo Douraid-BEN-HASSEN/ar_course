24f69a19933b2e041eb51dd3ffb61f290eef4d71
l.crouzil <laurent.crouzil@imerir.com>
2022-09-30 10:26:00 +0200

    commit

--- Engine/GObstacle.h
+++ Engine/GObstacle.h
@@ -1,11 +1,38 @@
 #ifndef GOBSTACLE_H
 #define GOBSTACLE_H
 
+#include <QGraphicsObject>
+#include <QObject>
+#include <QPainter>
+#include <QBrush>
 
-class GObstacle
+#include <Kart/Map/Obstacle.h>
+
+class GObstacle: public QGraphicsObject
 {
 public:
-    GObstacle();
+    GObstacle(qreal x, qreal y, qreal w, qreal h, QGraphicsItem *parent = nullptr);
+    GObstacle(Obstacle *, QGraphicsItem *parent = nullptr);
+
+    static qreal heigth;
+    static qreal width;
+    static qreal radius;
+
+    qreal getId() ;
+    qreal getX();
+    qreal getY();
+    qreal getHeigth();
+    qreal getWidth();
+    qreal getRadius();
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+
+private:
+    qreal id;
+    qreal x;
+    qreal y;
+
 };
 
 #endif // GOBSTACLE_H
