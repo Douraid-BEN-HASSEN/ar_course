5c1fdf3e305048b2b2da484bc1c10051fad2b7f3
pierre.ferer <pierre.ferer@imerir.com>
2022-09-30 16:52:53 +0200

    modification scene + modification déplacement des objetGraph + ajout Player

--- Client/2DGraphics/CheckpointGraphics.cpp
+++ Client/2DGraphics/CheckpointGraphics.cpp
@@ -35,7 +35,8 @@ QRectF CheckpointGraphics::boundingRect() const
 void CheckpointGraphics::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     painter->setBrush(Qt::green);
-    painter->drawEllipse(-this->getRadius(),-this->getRadius(),this->radius*2, this->radius*2);
+    //painter->drawEllipse(-this->getRadius(),-this->getRadius(),this->radius*2, this->radius*2);
+    painter->drawEllipse(boundingRect());
 
     painter->setPen(Qt::black);
     painter->drawText(0,0,QString::number(this->getId()));
