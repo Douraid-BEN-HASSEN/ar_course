1aa8f9456b5fcff4b3d8496b19f73543bb63e12e
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-10-02 17:09:11 +0200

    fix widget load

--- Client/interface.cpp
+++ Client/interface.cpp
@@ -1,24 +1,13 @@
 #include "interface.h"
 #include "ui_interface.h"
-#include<QFile>
-#include<QJsonObject>
-#include <QJsonArray>
-#include<QJsonDocument>
-#include <QDebug>
-#include <QMainWindow>
-#include <QObject>
-#include <QTcpSocket>
-#include <QArrayData>
-#include <QFileDialog>
-#include<QTextEdit>
-
-
 
 interface::interface(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::interface)
 {
     ui->setupUi(this);
+    Widget *widget = new Widget;
+    ui->verticalLayout->addWidget(widget);
 }
 
 interface::~interface()
--- Client/main.cpp
+++ Client/main.cpp
@@ -1,16 +1,11 @@
 #include <QApplication>
-
-#include <Mqtt/MqttService.h>
-#include <Kart/Map/Map.h>
-
-#include "2DGraphics/widget.h"
+#include "interface.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MqttService::instance();
 
-    Widget w;
+    interface w;
 
     w.show();
     return a.exec();
