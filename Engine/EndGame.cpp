0e94ed18c9a3cce3e2a03d36dd303b0c10a24878
l.crouzil <laurent.crouzil@imerir.com>
2022-10-12 19:48:21 +0200

    popUp endgame

--- Engine/EndGame.cpp
+++ Engine/EndGame.cpp
@@ -15,11 +15,25 @@ EndGame::~EndGame()
 
 void EndGame::setScoreboard(QWidget *widget)
 {
+    scoreBoard = widget;
+    //ui->verticalLayout_scorebord->widget();
     ui->verticalLayout_scorebord->addWidget(widget);
 }
 
 void EndGame::on_pushButton_clicked()
 {
     //signal de fermeture -> reset fenetre de jeux
+    //emit closeEndScore();
+    close();
+
 }
+
+void EndGame::closeEvent(QCloseEvent *){
+    emit closeEndScore();
+    //close();
+    //this->
+    //this->popUpEnd->setScoreboard(ui->tabWidget_scoreboard);
+    //popUpEnd->setModal(true);
+}
+
 
